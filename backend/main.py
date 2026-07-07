7196f469a46a16b41803a65f7755ab3d24a970c7
mattseq <matthewsequeir@gmail.com>
2026-07-06 20:25:54 -0500

    embedding model call; install openai stuff

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,27 @@
 import os
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+from langchain_openai import OpenAIEmbeddings
+from openai import OpenAI
 
 load_dotenv()
 
-embedding_model = init_chat_model(
-    "nvidia/nemotron-3-ultra-550b-a55b:free",
-    model_provider="openrouter"
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=os.environ.get("OPENROUTER_API_KEY"),
 )
 
-response = model.invoke("Why should I listen to you?")
+response = client.embeddings.create(
+  model="nvidia/llama-nemotron-embed-vl-1b-v2:free",
+  input=[
+    {
+      "content": [
+        {"type": "text", "text": "What is in this image?"}
+      ]
+    }
+  ],
+  encoding_format="float"
+)
 
-print(response)
+vector = response.data[0].embedding[:5]
+print(vector)
