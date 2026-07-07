de190ee7cd1bb2274d2314f29dc9715f85c0de65
mattseq <matthewsequeir@gmail.com>
2026-07-06 21:23:11 -0500

    chroma vectordb setup, install langchain-chroma

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
 
 from embeddings import NemotronEmbedding
 
@@ -11,7 +13,20 @@ client = OpenAI(
     api_key=os.environ.get("OPENROUTER_API_KEY"),
 )
 
-embeddings = NemotronEmbedding(client)
+embedding_function = NemotronEmbedding(client)
 
-vector = embeddings.embed_query("What is OpenRouter?")
+vector = embedding_function.embed_query("What is OpenRouter?")
 print(vector[:5])
+
+vector_store = Chroma(collection_name="pelion", embedding_function=embedding_function)
+
+document_1 = Document(page_content="i am hungry")
+document_2 = Document(page_content="openrouter gives me free ai models")
+document_3 = Document(
+    page_content="nvidia gives me free ai models but it steals all my data to train them"
+)
+
+documents = [document_1, document_2, document_3]
+vector_store.add_documents(documents)
+
+print(vector_store.similarity_search("food", 1))
