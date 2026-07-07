4419a54702200936038da5923c7cd7bd376b914d
mattseq <matthewsequeir@gmail.com>
2026-07-06 20:02:53 -0500

    add langchain-openrouter and basic model

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from backend!")
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+embedding_model = init_chat_model(
+    "nvidia/nemotron-3-ultra-550b-a55b:free",
+    model_provider="openrouter"
+)
+
+response = model.invoke("Why should I listen to you?")
+
+print(response)
