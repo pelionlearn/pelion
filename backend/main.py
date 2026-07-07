7629b60e059e20866fd37cf231972ef86c010ee6
krazyorange13 <77130315+krazyorange13@users.noreply.github.com>
2026-07-06 22:26:45 -0500

    k=7 search

--- backend/main.py
+++ backend/main.py
@@ -15,22 +15,29 @@ client = OpenAI(
 
 embedding_function = NemotronEmbedding(client)
 
-vector = embedding_function.embed_query("What is OpenRouter?")
-print(vector[:5])
-
+print("initializing vector store...")
 vector_store = Chroma(collection_name="pelion", embedding_function=embedding_function)
 
+print("adding documents...")
 document_1 = Document(page_content="i am hungry")
 document_2 = Document(page_content="openrouter gives me free ai models")
 document_3 = Document(
     page_content="nvidia gives me free ai models but it steals all my data to train them",
 )
-documents = [document_1, document_2, document_3]
-vector_store.add_documents(documents=documents, ids=["1", "2", "3"])
+document_4 = Document(
+    page_content="i love dogs! dogs have 4 legs and a tail. dogs are really cute and i love to pet them.",
+)
+documents = [document_1, document_2, document_3, document_4]
+vector_store.add_documents(documents=documents, ids=["1", "2", "3", "4"])
+
+print("adding images...")
 vector_store.add_images(
     uris=["images/images.jpeg", "images/images (1).jpeg", "images/images (2).jpeg"],
     ids=["cat", "dog", "camel"],
 )
 
-result = vector_store.similarity_search_with_score("dog", 1)[0]
-print(f"id: {result[0].id}, similarity score: {result[1]}")
+print("running similarity search...")
+results = vector_store.similarity_search_with_score("dog", 7)
+
+for result in results:
+    print(f"id: {result[0].id}, similarity score: {result[1]}")
