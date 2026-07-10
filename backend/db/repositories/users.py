dd775d5c862c2d5416d788339f5f4440537e82f2
mattseq <matthewsequeir@gmail.com>
2026-07-09 21:44:46 -0500

    repository methods

--- backend/db/repositories/users.py
+++ backend/db/repositories/users.py
@@ -2,17 +2,45 @@ from db.database import Session
 from db.models import User
 
 
-def create_user(name: str, email: str):
-    with Session() as session:
-        user = User(name, email)
+class UserRepository:
+    def create(self, name: str, email: str):
+        with Session() as session:
+            user = User(name, email)
 
-        session.add(user)
-        session.commit()
-        session.refresh(user)
+            session.add(user)
+            session.commit()
+            session.refresh(user)
 
-        return user
+            return user
 
+    def delete(self, id):
+        with Session as session:
+            user = session.get(User, id)
+            if user:
+                session.delete(user)
+                session.commit()
+            else:
+                return None
 
-def get_users():
-    with Session() as session:
-        return session.query(User).all()
+    def get(self, id):
+        with Session as session:
+            user = session.get(User, id)
+            return user
+
+    def get_all(self):
+        with Session() as session:
+            return session.query(User).all()
+
+    def rename(self, id, name: str):
+        with Session() as session:
+            user = session.get(User, id)
+
+            if user is None:
+                return None
+
+            user.name = name
+
+            session.commit()
+            session.refresh(user)
+
+            return user
