9c21ed7b1fd48679ae9690c2926ad7afde8e893e
mattseq <matthewsequeir@gmail.com>
2026-07-29 19:47:35 -0500

    fix register endpoint: change UserCreate schema

--- api/schemas/users.py
+++ api/schemas/users.py
@@ -1,13 +1,16 @@
 from pydantic import BaseModel
 from uuid import UUID
 from fastapi_users import schemas
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 
 class UserRead(schemas.BaseUser[UUID]):
     name: str
 
 
-class UserCreate(schemas.BaseUserCreate):
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
     name: str
 
 
