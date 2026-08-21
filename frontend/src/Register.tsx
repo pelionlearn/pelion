1cc2c2acccde37976f6b3b2903a2b2fd4dc582b4
mattseq <matthewsequeir@gmail.com>
2026-08-21 18:23:12 -0500

    frontend: register (and login after)

--- frontend/src/Register.tsx
+++ frontend/src/Register.tsx
@@ -2,15 +2,43 @@ import { useState, type ChangeEvent } from "react";
 import Navbar from "./Navbar.tsx";
 
 function Login() {
+    const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    async function handleSubmit(e: ChangeEvent<HTMLFormElement>) {
+    async function handleRegister(e: ChangeEvent<HTMLFormElement>) {
         e.preventDefault();
 
         try {
+            const response = await fetch("/api/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: email,
+                    name: username,
+                    password: password,
+                }),
+            });
+
+            if (response.ok) {
+                console.log("Registered");
+                handleLogin();
+            }
+
+            if (response.status == 400) {
+                console.log("Invalid credentials");
+            }
+        } catch (err) {
+            console.log("ummm");
+        }
+    }
+
+    async function handleLogin() {
+        try {
             const body = new URLSearchParams();
-            body.append("username", username);
+            body.append("username", email);
             body.append("password", password);
 
             const response = await fetch("/api/auth/login", {
@@ -33,6 +61,14 @@ function Login() {
         }
     }
 
+    async function handleRegisterWithGoogle() {
+        const response = await fetch("/api/auth/google/authorize");
+
+        const data = await response.json();
+
+        window.location.href = data.authorization_url;
+    }
+
     return (
         <div className="top-0 left-0 min-w-screen min-h-screen">
             <div className="relative z-10">
@@ -42,7 +78,7 @@ function Login() {
             <div className="flex items-center justify-center px-6 mt-20">
                 <div className="w-full max-w-md p-6 bg-background border border-dark rounded-2xl text-text">
                     <h2 className="text-2xl font-bold my-6 text-center">Create an Account</h2>
-                    <form onSubmit={handleSubmit} className="mb-4">
+                    <form onSubmit={handleRegister} className="mb-4">
                         <div className="py-2">
                             <div className="relative">
                                 <i
@@ -52,6 +88,21 @@ function Login() {
                                 <input
                                     type="email"
                                     placeholder="Email"
+                                    className="pl-10 px-3 py-2 w-full outline outline-dark rounded-lg"
+                                    onChange={e => setEmail(e.target.value)}
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <div className="py-2">
+                            <div className="relative">
+                                <i
+                                    className="fa-solid fa-user absolute left-3 top-1/2 -translate-y-1/2 text-text-secondary"
+                                    aria-hidden="true"
+                                />
+                                <input
+                                    type="username"
+                                    placeholder="Username"
                                     className="pl-10 px-3 py-2 w-full outline outline-dark rounded-lg"
                                     onChange={e => setUsername(e.target.value)}
                                     required
@@ -88,6 +139,7 @@ function Login() {
                     <div className="flex justify-center mt-4 gap-4">
                         <button
                             type="submit"
+                            onClick={handleRegisterWithGoogle}
                             className="w-full py-3 bg-background text-text rounded-2xl outline outline-dark cursor-pointer hover:-translate-y-0.5 transition-all duration-200"
                         >
                             <i className="fa-brands fa-google mr-2" />
