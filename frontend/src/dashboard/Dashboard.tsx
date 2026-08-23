fbb32d003c4308ec2bb90286c8018e9508eaf168
JeremySeq <jaysequeir@gmail.com>
2026-08-22 19:54:26 -0500

    frontend: use first letter of username as user icon

--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -90,7 +90,7 @@ function Dashboard() {
 
                         <button className="flex items-center gap-3 rounded-xl px-4 py-2">
                             <div className="flex h-10 w-10 items-center justify-center rounded-full bg-primary font-semibold text-black">
-                                J
+                                {user.name[0]}
                             </div>
 
                             <div>
